a21a22a9fc07b4b857acfbb4dc6f2b64b4f25394
Vipul Chaudhari <vipulchaudhary2005@gmail.com>
2023-12-18 14:42:38 +0530

    First Commit

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,32 @@
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [AppComponent],
+    });
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should create the app', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should have a valid title', () => {
+    expect(component.title).toBeTruthy();
+    expect(typeof component.title).toBe('string');
+    expect(component.title.length).toBeGreaterThan(10);
+  });
+
+  it('should have the correct title', () => {
+    // expect(component.title).toEqual('CustomerSessionPortal');
+    expect(component.title).toEqual('Retail-Banking-PC');
+  });
+
+});
